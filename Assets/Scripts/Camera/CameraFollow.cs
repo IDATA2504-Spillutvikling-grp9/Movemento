863575242bb1e26677aff08041fa3c44ff31f793
Håvard Hetland Vestbø <haavarhv@stud.ntnu.no>
2023-11-19 19:43:20 +0100

    Camera

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
@@ -7,17 +5,12 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private float followSpeed = 0.1f;
     [SerializeField] private Vector3 offset;
 
-
-
-    void Start()
+    private void LateUpdate()
     {
-
-    }
-
-
-
-    void Update()
-    {
-        transform.position = Vector3.Lerp(transform.position, PlayerController.Instance.transform.position + offset, followSpeed);
+        if (PlayerController.Instance != null)
+        {
+            Vector3 targetPosition = PlayerController.Instance.transform.position + offset;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed);
+        }
     }
 }
