cb95ebacb1d40d91205559bb2c8f9da279e7324a
Viddyyy <sanderknutsen125@gmail.com>
2023-11-29 18:51:49 +0100

    Working Boss

--- Assets/Scripts/Enemies/IslandBoss/StateMachine/Boss_Dive.cs
+++ Assets/Scripts/Enemies/IslandBoss/StateMachine/Boss_Dive.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Boss_Dive : StateMachineBehaviour
 {
     Rigidbody2D rb;
+    //bool callOnce;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -14,17 +16,30 @@ public class Boss_Dive : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+/*         BossDragonKnight.Instance.divingCollider.SetActive(true);
 
+        if (BossDragonKnight.Instance.Grounded())
+        {
+            BossDragonKnight.Instance.divingCollider.SetActive(false);
+
+            if(!callOnce)
+            {
+            BossDragonKnight.Instance.DivingPillars();
+            animator.SetBool("Dive", false);
+            BossDragonKnight.Instance.ResetAllAttacks();
+            callOnce = true;
+            }
+        } */
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        //callOnce = false;
     }
 
 
-    
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
