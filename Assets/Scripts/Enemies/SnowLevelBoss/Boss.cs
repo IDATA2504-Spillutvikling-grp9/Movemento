a3009ebe510adea4c04174f0e0fbc98226b740fb
Ole Kristian <oledole99@hotmail.no>
2023-11-30 14:02:00 +0100

    Added dockumentation to Boss script

--- Assets/Scripts/Enemies/SnowLevelBoss/Boss.cs
+++ Assets/Scripts/Enemies/SnowLevelBoss/Boss.cs
@@ -4,65 +4,85 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
+// Boss class inherits from the Enemy class
 public class Boss : Enemy
 {
-    
-   [SerializeField] private float chargStopTime;
-   [SerializeField] private float timeBetweenCharges;
-   [SerializeField] private float chaseDistance;
-   [SerializeField] private Animator doorAnimator;
+    // Serialized fields can be viewed and modified in the Unity Editor
 
-   private bool isCharging = false;
-   private float chargeTimer = 0f;
-   private float stopTimer = 0f;
+    [Header("Boss Properties")]
+    [SerializeField] private float chargStopTime;
+    [SerializeField] private float timeBetweenCharges;
+    [SerializeField] private float chaseDistance;
+    [SerializeField] private Animator doorAnimator;
 
-   private bool isDead = false;
+    // Private variables for internal use
+    private bool isCharging = false;
+    private float chargeTimer = 0f;
+    private float stopTimer = 0f;
+    private bool isDead = false;
 
-   protected override void Start() {
-    base.Start();
-    currentEnemyState = EnemyStates.Boss_Idle;
-    anim.SetBool("Idle", true);
-   }
-
-   protected override void Update() {
-    if(health <= 0) {
-        isDead = true;
-        OpenDoor();
-        anim.SetTrigger("Death");
-        base.Death(2f);
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        // Initialize boss-specific properties
+        currentEnemyState = EnemyStates.Boss_Idle;
+        anim.SetBool("Idle", true);
     }
-    UpdateEnemyStates();
-   }
 
-   private void OpenDoor() {
-    if(doorAnimator != null) {
-        doorAnimator.SetTrigger("Open");
-    }
-   }
-
-   protected override void UpdateEnemyStates() {
-    float _dist = Vector2.Distance(transform.position, PlayerController.Instance.transform.position);
-    switch (currentEnemyState) {
-        case EnemyStates.Boss_Idle:
-        anim.SetBool("Walk", false);
-        if(_dist < chaseDistance) {
-            BossIdleState();
-            anim.SetBool("Idle", true);
+    // Update is called once per frame
+    protected override void Update()
+    {
+        // Check if the boss is dead
+        if (health <= 0)
+        {
+            isDead = true;
+            OpenDoor();
+            anim.SetTrigger("Death");
+            // Call the Death method from the base class with a delay
+            base.Death(2f);
         }
-        break;
-
-        case EnemyStates.Boss_Chase:
-        if(isDead == true) {
-            return;
-        }
-        anim.SetBool("Idle", false);
-        BossChaseState();
-        anim.SetBool("Walk", true);
-        
-        break;
+        // Update the boss's state
+        UpdateEnemyStates();
     }
-   }
 
+    // Method to open the door (if a door animator is specified)
+    private void OpenDoor()
+    {
+        if (doorAnimator != null)
+        {
+            doorAnimator.SetTrigger("Open");
+        }
+    }
+
+    // Method to update the boss's state based on distance to the player
+    protected override void UpdateEnemyStates()
+    {
+        float _dist = Vector2.Distance(transform.position, PlayerController.Instance.transform.position);
+        switch (currentEnemyState)
+        {
+            case EnemyStates.Boss_Idle:
+                anim.SetBool("Walk", false);
+                if (_dist < chaseDistance)
+                {
+                    BossIdleState();
+                    anim.SetBool("Idle", true);
+                }
+                break;
+
+            case EnemyStates.Boss_Chase:
+                if (isDead == true)
+                {
+                    return;
+                }
+                anim.SetBool("Idle", false);
+                BossChaseState();
+                anim.SetBool("Walk", true);
+                break;
+        }
+    }
+
+    // Method representing the boss's idle state
     private void BossIdleState()
     {
         if (chargeTimer >= timeBetweenCharges)
@@ -76,6 +96,7 @@ public class Boss : Enemy
         }
     }
 
+    // Method representing the boss's chasing state
     private void BossChaseState()
     {
         float chargeStopTime = 2f;
@@ -84,6 +105,7 @@ public class Boss : Enemy
         {
             Vector2 directionToPlayer = (PlayerController.Instance.transform.position - transform.position).normalized;
 
+            // Flip the boss's scale based on the player's position
             if (directionToPlayer.x > 0)
             {
                 transform.localScale = new Vector3(-1.5f, 1.5f, 1.5f);
@@ -95,6 +117,7 @@ public class Boss : Enemy
 
             rb.velocity = directionToPlayer * speed;
 
+            // Stop charging after a certain duration
             if (stopTimer < chargeStopTime)
             {
                 stopTimer += Time.deltaTime;
@@ -109,6 +132,7 @@ public class Boss : Enemy
         else
         {
             float timeBetweenCharges = 3f;
+            // Resume charging after a certain duration
             if (stopTimer < timeBetweenCharges)
             {
                 stopTimer += Time.deltaTime;
@@ -118,6 +142,7 @@ public class Boss : Enemy
                 stopTimer = 0f;
                 isCharging = false;
 
+                // If the boss is almost stationary, switch back to idle state
                 if (rb.velocity.magnitude <= 0.01f)
                 {
                     currentEnemyState = EnemyStates.Boss_Idle;
